1b385d1c72de82f7d633ac98b770d571ef53c7e6
microshatter <2976466417@qq.com>
2026-08-25 13:19:33 +0800

    Increase max hash limit

--- scripts/utils/deduplicate.gd
+++ scripts/utils/deduplicate.gd
@@ -2,13 +2,14 @@ class_name Deduplicate
 extends RefCounted
 
 static var hash_list = PackedStringArray()
+static var MAX_HASH_LIMIT: int = 5000
 
 static func add_hash(hash_value: String):
 	if not is_in_hash(hash_value):
 		hash_list.append(hash_value)
 		print("Added hash %s. Total count %d" % [hash_value, len(hash_list)])
-	if len(hash_list) > 1000:
-		hash_list = hash_list.slice(hash_list.size() - 1000)
+	if len(hash_list) > MAX_HASH_LIMIT:
+		hash_list = hash_list.slice(hash_list.size() - MAX_HASH_LIMIT)
 		print(len(hash_list))
 
 static func is_in_hash(hash_value: String):
